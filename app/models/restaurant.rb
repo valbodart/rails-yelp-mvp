ad1b07a1c0e2ce374b77876f81bfab0f68e01e30
Valentine Bodart <valentine.bodart@gmail.com>
2022-02-16 12:08:55 +0100

    route created

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian)}
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian]}
   has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save
+end
