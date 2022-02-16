9014674c0449377be9486721c29a9eb050cf47a7
Valentine Bodart <valentine.bodart@gmail.com>
2022-02-16 14:18:14 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurant, except: %i[destroy edit update] do
-    resources :review, only: %i[new create]
+  resources :restaurants, except: %i[destroy edit update] do
+    resources :reviews, only: %i[new create]
   end
 end
